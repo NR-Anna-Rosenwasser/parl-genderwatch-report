c5bf2ff818379ac79d789267e9fb4ba1a62cf40a
Timothy Justin Oesch <timothy@kpunkt.ch>
2025-10-03 08:27:58 +0200

    Add route prefix to transcript stats

--- api/routes/api_v1.php
+++ api/routes/api_v1.php
@@ -4,34 +4,36 @@ use App\Models\ParlSession;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TranscriptStatsController;
 
-Route::get(
-    "basic-distribution/{parl_session:externalId}/",
-    function (ParlSession $parl_session) {
-        return (new TranscriptStatsController())->basicDistribution($parl_session);
-    }
-)->name('api.v1.stats.basicDistribution');
+Route::prefix("transcripts")->group(function () {
+    Route::get(
+        "basic-distribution/{parl_session:externalId}/",
+        function (ParlSession $parl_session) {
+            return (new TranscriptStatsController())->basicDistribution($parl_session);
+        }
+    )->name('api.v1.stats.transcripts.basicDistribution');
 
-Route::get(
-    "thematic-distribution/{parl_session:externalId}/",
-    function (ParlSession $parl_session) {
-        return (new TranscriptStatsController())->thematicDistribution($parl_session);
-    }
-)->name('api.v1.stats.thematicDistribution');
+    Route::get(
+        "thematic-distribution/{parl_session:externalId}/",
+        function (ParlSession $parl_session) {
+            return (new TranscriptStatsController())->thematicDistribution($parl_session);
+        }
+    )->name('api.v1.stats.transcripts.thematicDistribution');
 
-Route::get(
-    "cantonal-distribution/{parl_session:externalId}/",
-    function (ParlSession $parl_session) {
-        return (new TranscriptStatsController())->cantonalDistribution($parl_session);
-    })->name('api.v1.stats.cantonalDistribution');
+    Route::get(
+        "cantonal-distribution/{parl_session:externalId}/",
+        function (ParlSession $parl_session) {
+            return (new TranscriptStatsController())->cantonalDistribution($parl_session);
+        })->name('api.v1.stats.transcripts.cantonalDistribution');
 
-Route::get(
-    "age-distribution/{parl_session:externalId}/",
-    function (ParlSession $parl_session) {
-        return (new TranscriptStatsController())->ageDistribution($parl_session);
-    })->name('api.v1.stats.ageDistribution');
+    Route::get(
+        "age-distribution/{parl_session:externalId}/",
+        function (ParlSession $parl_session) {
+            return (new TranscriptStatsController())->ageDistribution($parl_session);
+        })->name('api.v1.stats.transcripts.ageDistribution');
 
-Route::get(
-    "group-distribution/{parl_session:externalId}/",
-    function (ParlSession $parl_session) {
-        return (new TranscriptStatsController())->groupDistribution($parl_session);
-    })->name('api.v1.stats.groupDistribution');
+    Route::get(
+        "group-distribution/{parl_session:externalId}/",
+        function (ParlSession $parl_session) {
+            return (new TranscriptStatsController())->groupDistribution($parl_session);
+        })->name('api.v1.stats.transcripts.groupDistribution');
+});
